e97760bb4b949606a654dfa0be5f4c6e7fe69b4a
HaoLong4935 <longsachse@gmail.com>
2026-02-25 21:24:56 +0700

    03: Database setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,8 @@ import type { Metadata } from "next";
 import { Inter, IBM_Plex_Mono } from "next/font/google";
 import { dark } from "@clerk/themes"
 import { ThemeProvider } from "@/components/theme-provider"
-
+import { Providers } from "@/components/providers";
 // Clerk
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
-
 
 import "./globals.css";
 
@@ -38,40 +29,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-        theme: dark,
-      }}
-    >
-      <html lang="en" suppressHydrationWarning>
-        <body
-          suppressHydrationWarning
-          className={`${inter.variable} ${plexmono.variable} antialiased`}
-        >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange>
-            <header className="flex justify-end items-center p-4 gap-4 h-16">
-              {/* Show the sign-in and sign-up buttons when the user is signed out */}
-              <SignedOut>
-                <SignInButton />
-                <SignUpButton>
-                  <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                    Sign Up
-                  </button>
-                </SignUpButton>
-              </SignedOut>
-              {/* Show the user button when the user is signed in */}
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </header>
+
+    <html lang="en" suppressHydrationWarning>
+      <body
+        suppressHydrationWarning
+        className={`${inter.variable} ${plexmono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange>
+          <Providers>
             {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+          </Providers>
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
